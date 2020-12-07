fb725d143690e39e78363147bac0c719ccc973d9
Nicklas <nicklascschmidt@Nicklass-MacBook-Pro.local>
2020-12-07 18:27:50 -0500

    simple page folder setup with Main, added component template

---
+++ src/components/template/template.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  
+`;
+
+class Name extends Component {
+  render() {
+    return (
+      <Container>
+      </Container>
+    )
+  }
+}
+
+export default Name;
