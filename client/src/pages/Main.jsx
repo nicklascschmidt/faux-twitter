b22f8d7b38f6910d184ca62e5c92a64ed1137394
Nicklas <nicklascschmidt@nicklass-mbp.lan>
2021-03-26 14:18:13 -0700

    added UnderMaintenance to Main

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Routes from '../routes/Routes';
 import Nav from '../components/Nav/Nav';
+import UnderMaintenance from './UnderMaintenance';
 
 const Container = styled.main`
   width: 100vw;
@@ -9,8 +10,32 @@ const Container = styled.main`
   grid-template-columns: 148px 600px;
 `;
 
+const InitialState = {};
+
+const isUnderMaintenance = true;
+
+/**TODO:
+ * add context provider for appState
+ * add context consumer to TweetInteraction comps for updating faves, RTs, comments (repl replies)
+ */
+
 class Main extends Component {
+  constructor(props) {
+    super(props);
+
+    // Grab prev state if exists
+    this.state = localStorage.getItem('appState')
+      ? JSON.parse(localStorage.getItem('appState'))
+      : InitialState;
+  }
+
+  componentWillUnmount() {
+    // Save state for next visit
+    localStorage.setItem('appState', JSON.stringify(this.state));
+  }
+
   render() {
+    if (isUnderMaintenance) return <UnderMaintenance />;
     return (
       <div>
         {this.props.children}
